0fe1650161a6d5001c9867291d52f538b7dbe99c
Md. Sabbir Alam <sabbiralam.cse@gmail.com>
2021-02-18 02:24:50 +0600

    Modified package.json and added webpack plugin to build the package.

--- dist/setup.js
+++ dist/setup.js
@@ -1,1 +1,124 @@
-!function(e,t){"object"==typeof exports&&"object"==typeof module?module.exports=t():"function"==typeof define&&define.amd?define([],t):"object"==typeof exports?exports.main=t():e.main=t()}(this,(function(){return function(e){var t={};function r(o){if(t[o])return t[o].exports;var i=t[o]={i:o,l:!1,exports:{}};return e[o].call(i.exports,i,i.exports,r),i.l=!0,i.exports}return r.m=e,r.c=t,r.d=function(e,t,o){r.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:o})},r.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},r.t=function(e,t){if(1&t&&(e=r(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var o=Object.create(null);if(r.r(o),Object.defineProperty(o,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var i in e)r.d(o,i,function(t){return e[t]}.bind(null,i));return o},r.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return r.d(t,"a",t),t},r.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},r.p="",r(r.s=4)}([function(e,t,r){"use strict";Object.defineProperty(t,"__esModule",{value:!0}),t.PACKAGE_NAME=t.accessToken=t.apiUrl=void 0,t.apiUrl="https://api.bitbucket.org/2.0/",t.accessToken=VARS&&VARS.BITBUCKET_PERSONAL_TOKEN,t.PACKAGE_NAME="bitbucket"},,,,function(e,t,r){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var o=r(0),i=null,n=prompt("What is your bitbucket username?");if(!n){var c="Username is required in order to setup bitbucket";throw notify(c,"error",6e3),c}if(args[0])i=args[0];else{open("https://bitbucket.org/account/settings/app-passwords/new");var u="workerb-"+(new Date).getTime();type(u,"#id_name",{method:"by_query_selector"}),click("account:write",{}),click("team:write",{}),click("project:write",{}),click("repository:write",{}),click("repository:admin",{}),click("repository:delete",{}),click("pipeline:variable",{}),click("webhook",{}),click("snippet:write",{}),click("wiki",{}),click("issue:write",{}),click("pullrequest:write",{}),click("Create",{expectReload:!0}),i=read(".app-passwords--password",{method:"by_query_selector"}),click("Close",{})}if(i){var s=btoa(n+":"+i);log(s),log(n),log(i),setVar(o.PACKAGE_NAME,[{name:"BITBUCKET_PERSONAL_TOKEN",value:s}]),notify("Access token added successfully.","success",3e3),reIndex()}else notify("Failed to save the auth token.","error",3e3)}])}));
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else if(typeof exports === 'object')
+		exports["main"] = factory();
+	else
+		root["main"] = factory();
+})(this, function() {
+return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/actions/setup.ts");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/actions/setup.ts":
+/*!******************************!*\
+  !*** ./src/actions/setup.ts ***!
+  \******************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+eval("\nObject.defineProperty(exports, \"__esModule\", { value: true });\nvar constants_1 = __webpack_require__(/*! ../utils/constants */ \"./src/utils/constants.ts\");\nvar appPassword = null;\nvar username = prompt(\"What is your bitbucket username?\");\nif (!username) {\n    var error = \"Username is required in order to setup bitbucket\";\n    notify(error, \"error\", 6000);\n    throw error;\n}\nif (args[0]) {\n    appPassword = args[0];\n}\nelse {\n    open(\"https://bitbucket.org/account/settings/app-passwords/new\");\n    var tokenName = \"workerb-\" + new Date().getTime();\n    type(tokenName, '#id_name', { method: 'by_query_selector' });\n    // give all the permissions\n    click('account:write', {});\n    click('team:write', {});\n    click('project:write', {});\n    click('repository:write', {});\n    click('repository:admin', {});\n    click('repository:delete', {});\n    click('pipeline:variable', {});\n    click('webhook', {});\n    click('snippet:write', {});\n    click('wiki', {});\n    click('issue:write', {});\n    click('pullrequest:write', {});\n    click('Create', {\n        expectReload: true\n    });\n    appPassword = read('.app-passwords--password', { method: 'by_query_selector' });\n    click('Close', {});\n}\nif (!appPassword) {\n    notify('Failed to save the auth token.', 'error', 3000);\n}\nelse {\n    var token = btoa(username + \":\" + appPassword);\n    log(token);\n    log(username);\n    log(appPassword);\n    setVar(constants_1.PACKAGE_NAME, [\n        {\n            name: 'BITBUCKET_PERSONAL_TOKEN',\n            value: token,\n        },\n    ]);\n    notify('Access token added successfully.', 'success', 3000);\n    reIndex();\n}\n\n\n//# sourceURL=webpack://main/./src/actions/setup.ts?");
+
+/***/ }),
+
+/***/ "./src/utils/constants.ts":
+/*!********************************!*\
+  !*** ./src/utils/constants.ts ***!
+  \********************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+eval("\nObject.defineProperty(exports, \"__esModule\", { value: true });\nexports.PACKAGE_NAME = exports.accessToken = exports.apiUrl = void 0;\nexports.apiUrl = 'https://api.bitbucket.org/2.0/';\nexports.accessToken = VARS && VARS[\"BITBUCKET_PERSONAL_TOKEN\"];\nexports.PACKAGE_NAME =  false ? undefined : \"dist\";\n\n\n//# sourceURL=webpack://main/./src/utils/constants.ts?");
+
+/***/ })
+
+/******/ });
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,73 +1,93 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const webpack = require("webpack");
+const fs = require("fs");
+const helpers = require("./webpack.helpers.js");
+const WBMetaJsonGenerator = require("wb-packager-webpack-plugin");
 
-const path = require('path')
-const CopyPlugin = require('copy-webpack-plugin');
-const webpack = require('webpack');
-const fs = require('fs');
-const helpers = require('./webpack.helpers.js')
+const fileSystem = helpers.generateFS(__dirname + "/src/actions", "workerB");
 
+const entryFiles = helpers.generateEntryPaths(fileSystem.children);
 
-const fileSystem = helpers.generateFS(__dirname + '/src/actions', "workerB")
+const entryPaths = helpers
+  .getFiles(entryFiles, ".ts")
+  .map((file) => file.replace(".ts", ""));
 
-const entryFiles = helpers.generateEntryPaths(fileSystem.children)
+const metaFiles = helpers.getFiles(entryFiles, ".json");
 
-const entryPaths = helpers.getFiles(entryFiles, ".ts").map(file => file.replace('.ts', ''))
+console.log(metaFiles);
 
-const metaFiles = helpers.getFiles(entryFiles, ".json")
+let copyPatterns = metaFiles.map((metaFile) => ({
+  from: "./src/actions/" + metaFile,
+  to: "./" + metaFile,
+}));
 
-console.log(metaFiles)
-
-let copyPatterns = metaFiles.map(
-    (metaFile) => ({ from: './src/actions/' + metaFile, to: './' + metaFile })
-)
-
-const rootJSON = fs.readFileSync("./src/actions/meta.json", 'utf8')
-const rootJSONParsed = rootJSON ? JSON.parse(rootJSON) : {}
-let iconPath = ""
+const rootJSON = fs.readFileSync("./src/actions/meta.json", "utf8");
+const rootJSONParsed = rootJSON ? JSON.parse(rootJSON) : {};
+let iconPath = "";
 
 if (rootJSONParsed.icon) {
-    iconPath = path.join("./src/actions", rootJSONParsed.icon)
-    copyPatterns.concat({ from: iconPath, to: './' })
+  iconPath = path.join("./src/actions", rootJSONParsed.icon);
+  copyPatterns.concat({ from: iconPath, to: "./" });
 }
 
 module.exports = {
-    entry: entryPaths.reduce((result, entryPath) => {
-        result[entryPath] = "./src/actions/" + entryPath + ".ts"
-        return result;
-    }, {}),
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js',
-        globalObject: 'this',
-        libraryTarget: 'umd',
-        library: 'main',
+  entry: entryPaths.reduce((result, entryPath) => {
+    result[entryPath] = "./src/actions/" + entryPath + ".ts";
+    return result;
+  }, {}),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+    globalObject: "this",
+    libraryTarget: "umd",
+    library: "main",
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+    alias: {
+      utils: path.resolve(__dirname, "src/utils/"),
     },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-        alias: {
-            utils: path.resolve(__dirname, 'src/utils/'),
-        }
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.js?$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-                query: { "presets": ["@babel/preset-env"] }
-            }
-        ]
-    },
-    plugins: [
-        new CopyPlugin({
-          patterns: copyPatterns,
-          options: {
-            concurrency: 100,
-          },
-        })
-    ]
-}
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        query: { presets: ["@babel/preset-env"] },
+      },
+    ],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: copyPatterns,
+      options: {
+        concurrency: 100,
+      },
+    }),
+    new WBMetaJsonGenerator({
+      environment: "development",
+      package: "wb-bitbucket",
+      packageDescription: "test",
+      packageIcon: "",
+      readmeFile: "README.md",
+      folderDescriptionList: [
+        {
+          path: "/boards",
+          description: "Display all the boards",
+          iconPath: "",
+        },
+        {
+          path: "",
+          description: "Display all the lists of the board",
+        },
+      ],
+    }),
+  ],
+};
